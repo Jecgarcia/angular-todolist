09c9a791f5dd855fbcd7dd3869c5e10265e9d099
Jessica Garcia Chaparro <jessicagarciachaparro@gmail.com>
2023-12-11 23:08:01 -0500

    Añadir primeros experimentos de paginas y routing en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-proyecto';
+  btnDisabled = true; 
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled; 
+  }
 }
