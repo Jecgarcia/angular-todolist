cf5940f7e30a9331b704f01fece5ab7de786f4d4
Jessica Garcia Chaparro <jessicagarciachaparro@gmail.com>
2023-12-13 22:27:37 -0500

    Manejando estructura de control ngFor con array

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Task} from '../../models/task.model'
+import { title } from 'process';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +11,47 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  tasks = signal<Task[]>(
+    [
+      {
+        id: 1, 
+        title: 'Instalar Angular CLI',  
+        completed: false
+      },
+      {
+        id: 2, 
+        title: 'Crear proyecto',  
+        completed: false
+      },
+      {
+        id: 3, 
+        title: 'Crear compponente',  
+        completed: false
+      },
+      {
+        id: 4, 
+        title: 'Crear servicio',  
+        completed: false
+      } 
+    ]
+  ); 
 
+  changeHandler (event: Event) {
+    const input = event.target as HTMLInputElement; 
+    const newTask = input.value; 
+    this.addTask(newTask); 
+  }
+
+  addTask (title:string) {
+    const newTask = {
+      id: Date.now(), 
+      title, 
+      completed: false, 
+    }; 
+    this.tasks.update((tasks) => [...tasks, newTask]);  
+  }
+
+  deleteTask (index:number) {
+    this.tasks.update(tasks => tasks.filter((task, position) => position !== index)); 
+  }
 }
